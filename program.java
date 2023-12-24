e0ff8ff251545a338302f3d27dba9eae15c160bb
Max <Bruce.97@mail.ru>
2023-12-24 09:34:03 +0700

    add compiled files and comment (how it works)

--- program.java
+++ program.java
@@ -1,5 +1,13 @@
 //  /usr/lib/jvm/jdk-20/bin/java program.java
 
+// Компиляция
+// javac -d bin program.java 
+
+// Запуск скомпилированного
+// Обрпщпться к пакету чере точку, не через слеш
+// java -classpath ./bin program
+
+
 import testLib.lib;
 
 public class program {
